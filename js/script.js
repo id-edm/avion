aa1b4e90138ca7567f6db7d8245bcaddbf9a0052
Derevjannykh Ivan <ivandubochit@gmail.com>
2024-05-23 13:04:29 +0300

    feat: updated stepper counter

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const noticeEl = document.querySelector(".notice");
-const stepperEl = document.querySelector(".stepper");
+const stepperEls = document.querySelectorAll(".stepper");
 
 if (noticeEl) {
   const noticeCloseEl = noticeEl.querySelector(".notice__close");
@@ -8,49 +8,57 @@ if (noticeEl) {
   });
 }
 
-if (stepperEl) {
-  const stepperInputEl = stepperEl.querySelector(".stepper__input");
-  const stepperBtnMinusEl = stepperEl.querySelector(".stepper__btn--minus");
-  const stepperBtnPlusEl = stepperEl.querySelector(".stepper__btn--plus");
+if (stepperEls) {
+  stepperEls.forEach((stepperEl) => {
+    const stepperInputEl = stepperEl.querySelector(".stepper__input");
+    const stepperBtnMinusEl = stepperEl.querySelector(".stepper__btn--minus");
+    const stepperBtnPlusEl = stepperEl.querySelector(".stepper__btn--plus");
 
-  const stepperMin = Number(stepperInputEl.getAttribute("min"));
-  const stepperMax = Number(stepperInputEl.getAttribute("max"));
+    const stepperMin = Number(stepperInputEl.getAttribute("min"));
+    const stepperMax = Number(stepperInputEl.getAttribute("max"));
 
-  let count = Number(stepperInputEl.value);
+    let count = Number(stepperInputEl.value);
 
-  const updateStepperButtons = () => {
-    stepperBtnMinusEl.classList.toggle("stepper__btn--disabled", count <= stepperMin);
-    stepperBtnPlusEl.classList.toggle("stepper__btn--disabled", count >= stepperMax);
-  };
+    const updateStepperButtons = () => {
+      stepperBtnMinusEl.classList.toggle(
+        "stepper__btn--disabled",
+        count <= stepperMin
+      );
+      stepperBtnPlusEl.classList.toggle(
+        "stepper__btn--disabled",
+        count >= stepperMax
+      );
+    };
 
-  stepperInputEl.addEventListener("change", () => {
-    count = Number(stepperInputEl.value);
+    stepperInputEl.addEventListener("change", () => {
+      count = Number(stepperInputEl.value);
 
-    if (count < stepperMin) {
-      count = stepperMin;
-    } else if (count > stepperMax) {
-      count = stepperMax;
-    }
+      if (count < stepperMin) {
+        count = stepperMin;
+      } else if (count > stepperMax) {
+        count = stepperMax;
+      }
 
-    stepperInputEl.value = count;
-    updateStepperButtons();
-  });
-
-  stepperBtnMinusEl.addEventListener("click", () => {
-    if (count > stepperMin) {
-      count--;
       stepperInputEl.value = count;
       updateStepperButtons();
-    }
-  });
+    });
 
-  stepperBtnPlusEl.addEventListener("click", () => {
-    if (count < stepperMax) {
-      count++;
-      stepperInputEl.value = count;
-      updateStepperButtons();
-    }
-  });
+    stepperBtnMinusEl.addEventListener("click", () => {
+      if (count > stepperMin) {
+        count--;
+        stepperInputEl.value = count;
+        updateStepperButtons();
+      }
+    });
 
-  updateStepperButtons(); // Инициализация состояния кнопок при загрузке
+    stepperBtnPlusEl.addEventListener("click", () => {
+      if (count < stepperMax) {
+        count++;
+        stepperInputEl.value = count;
+        updateStepperButtons();
+      }
+    });
+
+    updateStepperButtons(); // Инициализация состояния кнопок при загрузке
+  });
 }
